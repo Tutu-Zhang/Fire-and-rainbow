ac9bfccf9638a7e298fd4ff76b4818ddb1a277ab
Targaryer <3422642381@qq.com>
2022-10-26 03:15:06 +0800

    v1.01

--- Assets/script/UI/UI-button/ClickToNext.cs
+++ Assets/script/UI/UI-button/ClickToNext.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ClickToNext : MonoBehaviour
 {
     private GameObject clickToNext_button;
-    private int count = 1;
+    private int count = 0;
 
     void Start()
     {
@@ -22,14 +22,16 @@ public class ClickToNext : MonoBehaviour
         }
 
         //很麻烦的写法，但是比再建一个txt简单多了
-        PlayerPrefs.SetString("lv0guide1", "在赛博电子世界，我们会用网络攻击来战斗，而攻击形式会被具象为具体的形象。");
+        PlayerPrefs.SetString("lv0guide1", "在赛博黑客的世界， [信息] 就是我们的武器。而在计算机中，信息便是由 [0] 和 [1] 组成。");
         PlayerPrefs.SetString("lv0guide2", "这便是你所看到的战斗界面。");
-        PlayerPrefs.SetString("lv0guide3", "下面8位是信源区，每回合会有60%概率填充0，40%概率填充1。");
-        PlayerPrefs.SetString("lv0guide4", "上面4个位置是信道区，将信源区的数字放入其中组合，可发动总计16种不同的技能，具体效果会在左上角的AI面板中写出。");
+        PlayerPrefs.SetString("lv0guide3", "下面8位是 [信源区] ，每回合会有60%概率填充0，40%概率填充1。");
+        PlayerPrefs.SetString("lv0guide4", "上面4个位置是 [信道区] ，将信源区的数字放入其中组合，可发动多种不同的技能");
         PlayerPrefs.SetString("lv0guide5", "组合的技巧在于，技能组合中1的数量越多，技能效果越强力。");
         PlayerPrefs.SetString("lv0guide6", "以1开头的组合更倾向于攻击，而0开头的组合则更倾向于防守。");
-        PlayerPrefs.SetString("lv0guide7", "不同的敌人需要不同的策。熟悉技能效果，合理规划资源，使用智慧战胜敌人吧！（点击任意位置结束教程）");
-        //PlayerPrefs.SetString("lv0guide0", "进入加密通话，已为您暂时屏蔽敌人");
+        PlayerPrefs.SetString("lv0guide7", "你将会面对不同的敌人。他们每个都有独门绝技，斗志和耐性更是技惊四座");
+        PlayerPrefs.SetString("lv0guide8", "幸运的是，我会在战斗中辅助您，帮您显示不同组合的效果");
+        PlayerPrefs.SetString("lv0guide9", "不幸的是...屏蔽效果即将失效。    敌人来袭！");
+        PlayerPrefs.SetString("lv0guide0", "情况紧急，已为您暂时屏蔽敌人。接下来我将为您简单回忆战斗逻辑");
         PlayerPrefs.Save();
 
 
@@ -39,7 +41,7 @@ public class ClickToNext : MonoBehaviour
 
     private void NextGuide()
     {
-        if (count <= 7 && LevelManager.Instance.level == 0)
+        if (count <= 9 && LevelManager.Instance.level == 0)
         {
             GameObject guideText = GameObject.Find("/Canvas/GameWindow/guide");
             guideText.GetComponent<Text>().text = PlayerPrefs.GetString("lv0guide" + count.ToString());
